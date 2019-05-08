c78d7532d363689f9ef9fa35b6f20133b74c1128
LuK1337 <priv.luk@gmail.com>
2019-05-09 01:53:00 +0200

    guacamole: Enable aux camera for Snap
    Change-Id: I385bc59359f21b269390e3941f32436b362627b2

--- device.mk
+++ device.mk
@@ -10,7 +10,8 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Camera
 PRODUCT_PACKAGES += \
