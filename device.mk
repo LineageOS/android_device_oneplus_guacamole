09dc8a53d35ec3862bfa95c434b0c1ac4eea6590
Peter Cai <peter@typeblog.net>
2019-06-19 12:12:40 +0800

    guacamole: build fingerprint service

--- device.mk
+++ device.mk
@@ -45,6 +45,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+PRODUCT_PACKAGES += \
+	android.hardware.biometrics.fingerprint@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
