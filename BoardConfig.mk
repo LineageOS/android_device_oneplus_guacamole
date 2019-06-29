ae095297ae9116df85cded407ae6d07c8d111ddd
Peter Cai <peter@typeblog.net>
2019-06-29 08:57:52 +0800

    guacamole: move motor in matrix back to common
    * it seems that the AOSP build system doesn't allow two different device
    framework compatibility matrices with the same level. Move it back.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,5 +38,4 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Treble
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
