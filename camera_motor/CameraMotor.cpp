8f3576fd47264ba30c1cb84372d351f52d31efa1
LuK1337 <priv.luk@gmail.com>
2019-07-01 22:26:37 +0200

    guacamole: camera_motor: Block while motor changes state
    Change-Id: Ibb13df9a630184e2c5af75f17f23965c7a0be8c2

--- camera_motor/CameraMotor.cpp
+++ camera_motor/CameraMotor.cpp
@@ -19,12 +19,17 @@
 #include "CameraMotor.h"
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
+#include <chrono>
 #include <fstream>
+#include <thread>
 
 #define CAMERA_MOTOR_ENABLE "/sys/devices/platform/vendor/vendor:motor_pl/enable"
 #define CAMERA_MOTOR_DIRECTION "/sys/devices/platform/vendor/vendor:motor_pl/direction"
+#define CAMERA_MOTOR_POSITION "/sys/devices/platform/vendor/vendor:motor_pl/position"
 #define DIRECTION_DOWN "0"
 #define DIRECTION_UP "1"
+#define POSITION_DOWN "1"
+#define POSITION_UP "0"
 #define ENABLED "1"
 #define CAMERA_ID_FRONT 1
 
@@ -34,6 +39,8 @@ namespace camera {
 namespace motor {
 namespace V1_0 {
 namespace implementation {
+
+using namespace std::chrono_literals;
 
 /*
  * Write value to path and close file.
@@ -53,23 +60,55 @@ static T get(const std::string& path, const T& def) {
     return file.fail() ? def : result;
 }
 
+static void waitUntilFileChange(const std::string& path, const std::string &val,
+        const std::chrono::milliseconds relativeTimeout) {
+    auto startTime = std::chrono::steady_clock::now();
+
+    while (true) {
+        if (get<std::string>(path, "") == val) {
+            return;
+        }
+
+        std::this_thread::sleep_for(50ms);
+
+        auto now = std::chrono::steady_clock::now();
+        auto timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime);
+
+        if (timeElapsed > relativeTimeout) {
+            return;
+        }
+    }
+}
+
 Return<void> CameraMotor::onConnect(int8_t cameraId) {
-    if (cameraId == CAMERA_ID_FRONT) {
+    auto motorPosition = get<std::string>(CAMERA_MOTOR_POSITION, "");
+
+    if (cameraId == CAMERA_ID_FRONT && motorPosition == POSITION_DOWN) {
         LOG(INFO) << "Popping out front camera";
 
         set(CAMERA_MOTOR_DIRECTION, DIRECTION_UP);
         set(CAMERA_MOTOR_ENABLE, ENABLED);
+        waitUntilFileChange(CAMERA_MOTOR_POSITION, POSITION_UP, 5s);
+    } else if (cameraId != CAMERA_ID_FRONT && motorPosition == POSITION_UP) {
+        LOG(INFO) << "Retracting front camera";
+
+        set(CAMERA_MOTOR_DIRECTION, DIRECTION_DOWN);
+        set(CAMERA_MOTOR_ENABLE, ENABLED);
+        waitUntilFileChange(CAMERA_MOTOR_POSITION, POSITION_DOWN, 5s);
     }
 
     return Void();
 }
 
 Return<void> CameraMotor::onDisconnect(int8_t cameraId) {
-    if (cameraId == CAMERA_ID_FRONT) {
+    auto motorPosition = get<std::string>(CAMERA_MOTOR_POSITION, "");
+
+    if (cameraId == CAMERA_ID_FRONT && motorPosition == POSITION_UP) {
         LOG(INFO) << "Retracting front camera";
 
         set(CAMERA_MOTOR_DIRECTION, DIRECTION_DOWN);
         set(CAMERA_MOTOR_ENABLE, ENABLED);
+        waitUntilFileChange(CAMERA_MOTOR_POSITION, POSITION_DOWN, 5s);
     }
 
     return Void();
