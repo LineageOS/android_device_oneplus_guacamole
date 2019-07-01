4475ef5476f481b125af0cdf38c018d810a58df7
LuK1337 <priv.luk@gmail.com>
2019-07-01 13:51:28 +0200

    guacamole: Convert tabs to spaces in makefiles
    Change-Id: I08c08ffa84f317651358f4aa96834b421bdaae43

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_guacamole.mk
+    $(LOCAL_DIR)/lineage_guacamole.mk
--- device.mk
+++ device.mk
@@ -7,21 +7,21 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-	$(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=560 \
-	persist.vendor.sensors.light.location_x=1000 \
-	persist.vendor.sensors.light.location_y=260 \
-	persist.vendor.sensors.light.full_white_reading=200
+    ro.sf.lcd_density=560 \
+    persist.vendor.sensors.light.location_x=1000 \
+    persist.vendor.sensors.light.location_y=260 \
+    persist.vendor.sensors.light.full_white_reading=200
 
 PRODUCT_PACKAGES += \
-	init.display.guacamole.rc
+    init.display.guacamole.rc
 
 # Camera
 PRODUCT_PACKAGES += \
-	vendor.lineage.camera.motor@1.0 \
-	vendor.lineage.camera.motor@1.0-service.oneplus_msmnile
+    vendor.lineage.camera.motor@1.0 \
+    vendor.lineage.camera.motor@1.0-service.oneplus_msmnile
