266bada0cc3def0b5cc78fe70973575953429cb1
LuK1337 <priv.luk@gmail.com>
2019-07-04 00:54:59 +0200

    guacamole: Sync lineage_guacamole.mk with pioneer's layout
    Change-Id: I0bfafdc7e46db7e38aefdeaa1725f3c5f10e2055

--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -4,7 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
