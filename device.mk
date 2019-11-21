ff3f72db81bcb61c4e683464e3de404de52491aa
LuK1337 <priv.luk@gmail.com>
2019-11-21 22:24:36 +0100

    guacamole: Move camera_* to sm8150-common

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Camera
 PRODUCT_PACKAGES += \
     vendor.lineage.camera.motor@1.0 \
-    vendor.lineage.camera.motor@1.0-service.guacamole \
+    vendor.lineage.camera.motor@1.0-service.oneplus_msmnile \
     OnePlusCameraHelper
 
 # Device init scripts
