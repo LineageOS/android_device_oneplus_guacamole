2e66c4102d88bcd6b08231d2a3a4fd24aa0470f1
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-16 14:19:50 +0000

    guacamole: Set correct model number
    Set to what is present on stock.
    Set to GM1917 (Global) since it makes more sense as an enforced model.
    Enables HDR playback in Netflix since it probably queries a database and only if its present does it enable the feature.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Ic4bb9e8517be4a5cebd431c3412cfa6e6f2b2fd1

--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -18,7 +18,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7 Pro
+PRODUCT_MODEL := GM1917
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
