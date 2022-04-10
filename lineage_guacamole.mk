ddb7bf423fddfa8aef98032403c5feb4ea7d99e8
Bruno Martins <bgcngm@gmail.com>
2022-04-10 01:20:44 -0700

    guacamole: Update blobs to Android 12 (GM1911_11.H.28)
    Change-Id: Ib0843bfc2e802af16853875ae4649c403898f51f

--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -18,7 +18,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := GM1917
+PRODUCT_MODEL := GM1911
 PRODUCT_BRAND := OnePlus
 
 PRODUCT_SYSTEM_NAME := OnePlus7Pro
@@ -27,8 +27,8 @@ PRODUCT_SYSTEM_DEVICE := OnePlus7Pro
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus7Pro-user 12 SKQ1.211113.001 P.202209220221 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus7Pro/OnePlus7Pro:12/SKQ1.211113.001/P.202209220221:user/release-keys
