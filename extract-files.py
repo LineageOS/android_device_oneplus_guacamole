ce172432566b6e5dc3ab483eddbdcbc7e9c16b95
LuK1337 <priv.luk@gmail.com>
2024-10-13 18:08:25 +0200

    eepy
    Change-Id: I02baf990b1d151ec5b9cfaf89e5c6ab94250bf82

---
+++ extract-files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+from extract_utils.fixups_blob import (
+    blob_fixups_user_type,
+    blob_fixup,
+)
+
+namespace_imports = [
+    'vendor/oneplus/sm8150-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/hw/audio.primary.msmnile.so': blob_fixup().replace_needed('/vendor/lib/liba2dpoffload.so', '/odm/lib/liba2dpoffload.so'),
+}
+
+module = ExtractUtilsModule(
+    'guacamole',
+    'oneplus',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8150-common', module.vendor
+    )
+    utils.run()
